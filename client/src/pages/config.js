ed22a5353dcd32f884e2452054c67491f2ba5531
Satyam Maurya <satyammaurya1001@gmail.com>
2026-04-07 02:52:09 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,7 +1,9 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://381491941344.realhandsonlabs.net"
+// const API_BASE_URL = "http://381491941344.realhandsonlabs.net"
 
-// const API_BASE_URL = "http://98.92.112.243:84"
+// ansible server public IP
+const API_BASE_URL = "http://44.204.225.200:84"   
+
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
