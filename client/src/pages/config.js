1ccdf614763cea84cdb4bfb831f4633972cd48b9
Satyam Maurya <satyammaurya1001@gmail.com>
2026-04-07 03:20:01 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,8 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
 // const API_BASE_URL = "http://381491941344.realhandsonlabs.net"
 
-// ansible server public IP
-const API_BASE_URL = "http://44.204.225.200:84"   
+// ansible target node public IP
+const API_BASE_URL = "http://44.214.6.75:84"   
 
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
